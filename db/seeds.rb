be6d04013ee68eb4bd70dcbde3a973ce1a826bef
motoki <81569237+motokiteruya@users.noreply.github.com>
2021-05-14 22:49:40 +0900

    seeds.rbの値修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 require "faker"
 Faker::Config.locale = :ja
 
-posts_list = []
+post_lists = []
 
 # 50件の初期データを投入
 50.times do
-  posts_list << { body: Faker::name.title}
+  post_lists << { body: Faker::Book.title}
 end
 
-Post.create!(posts_list)
+Post.create!(post_lists)
